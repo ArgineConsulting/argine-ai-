4df6ac74734afabe5851bed462829e83e0e96a40
expatbrat888 <aronalds@gmail.com>
2023-11-16 13:33:30 +0100

    first push

---
+++ gatsby-config.ts
@@ -0,0 +1,49 @@
+import type { GatsbyConfig } from 'gatsby'
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: 'argine',
+    siteUrl: 'https://argine.digital/',
+    description: 'argine description',
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "argine.digital",
+        short_name: "argine",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: 'use-credentials',
+      },
+    },
+  ],
+}
+
+export default config
---
+++ netlify/functions/mail.ts
@@ -0,0 +1,58 @@
+import { createTransport } from 'nodemailer'
+
+export default async function handler(req: Request) {
+  try {
+    // Retrieve SMTP configuration from environment variables
+    const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env
+
+    if (!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASSWORD) {
+      throw new Error('SMTP configuration missing in environment variables.')
+    }
+
+    // Check request method
+    if (req.method !== 'POST') {
+      return new Response('Method Not Allowed', { status: 405 })
+    }
+
+    // Read and parse the request body
+    const bodyText = await req.text() // Use await to read the entire stream
+    const requestBody = JSON.parse(bodyText) // Assuming the body is in JSON format
+
+    // Extract data from the parsed request body
+    const { email, subject, message } = requestBody
+
+    // Create a Nodemailer transporter
+    const transporter = createTransport({
+      service: 'gmail',
+      port: Number(SMTP_PORT),
+      host: SMTP_HOST,
+      secure: false,
+      auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD,
+      },
+    })
+
+    const text = `
+      From: ${email}
+      Goal: ${subject}
+      Message: ${message}
+    `
+
+    // Define email configuration
+    const mailConfig = {
+      to: SMTP_USER,
+      subject: 'Argine.digital Contact Form Request',
+      text,
+    }
+
+    // Try to send the email
+    await transporter.sendMail(mailConfig)
+
+    // Email sent successfully
+    return new Response('Email sent successfully', { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return new Response('Error sending email', { status: 500 })
+  }
+}
